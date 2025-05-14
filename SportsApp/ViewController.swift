0b85d2d3eea804849ddb66f76ec3cc76e201f85b
Khairy Suleiman Jr <khairy.hatem4@gmail.com>
2025-05-14 21:46:34 +0200

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Khairy on 14/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
